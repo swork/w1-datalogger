227949eb1cff2b5e58138050c86a3d15d2e8db2f
Steve Work <steve@work.renlabs.com>
2020-01-30 19:00:52 -0800

    v0.1.1, for version screwup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="w1-datalogger",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     author="Steve Work",
     author_email="steve@work.renlabs.com",
