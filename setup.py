09a6b7806a329f654cf6918964f207a5c239fabc
Steve Work <steve@work.renlabs.com>
2020-01-30 18:58:44 -0800

    bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="w1-datalogger",
-    version="0.0.1",
+    version="0.1.0",
     packages=find_packages(),
     author="Steve Work",
     author_email="steve@work.renlabs.com",
